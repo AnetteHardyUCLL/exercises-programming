65064c6ca8f278e4575b9be0df6afca2769d3f8d
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-02-15 19:05:26 +0100

    write a function that returns the median of a list

--- 01-basic-python/04-lists/05-median/student.py
+++ 01-basic-python/04-lists/05-median/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+
+
+def median(ns):
+    ns.sort()
+    middle_value = len(ns) // 2
+    if len(ns) % 2 != 0:
+        return ns[middle_value]
+    else:
+        return (ns[middle_value] + ns[(middle_value - 1)]) / 2
