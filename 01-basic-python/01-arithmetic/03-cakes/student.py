6848ab31e59560aeeecedfc88adf7f46dca11144
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-02-15 12:45:51 +0100

    write a function that given a number of ingredients, returns the number of cakes that can be baked

--- 01-basic-python/01-arithmetic/03-cakes/student.py
+++ 01-basic-python/01-arithmetic/03-cakes/student.py
@@ -1,1 +1,10 @@
 # Write your code here
+
+
+def cakes(eggs, butter, flour):
+    cakes_eggs = eggs // 5
+    cakes_butter = butter // 250
+    cakes_flour = flour // 250
+    total_cakes = min(cakes_eggs, cakes_butter, cakes_flour)
+
+    return total_cakes
