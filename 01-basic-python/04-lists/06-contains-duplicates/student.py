161886f02cf3dc7cb061ec1af1d14e733c4916a3
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-02-15 19:12:36 +0100

    write a function that checks if a given list contains duplicates

--- 01-basic-python/04-lists/06-contains-duplicates/student.py
+++ 01-basic-python/04-lists/06-contains-duplicates/student.py
@@ -1,1 +1,9 @@
 # Write your code here
+
+
+def contains_duplicates(xs):
+    sorted_list = sorted(xs)
+    for i in range(len(sorted_list) - 1):
+        if sorted_list[i] == sorted_list[i + 1]:
+            return True
+    return False
