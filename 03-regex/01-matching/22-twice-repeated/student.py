eea1bfe939a917758c5038c3b3495041637bdc96
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-03-12 13:34:05 +0100

    finished 28 exercises

--- 03-regex/01-matching/22-twice-repeated/student.py
+++ 03-regex/01-matching/22-twice-repeated/student.py
@@ -1,1 +1,11 @@
-# Write your code here
+import re
+
+
+# using raw strings
+def twice_repeated(string):
+    return re.fullmatch(r"(.+)\1", string)
+
+
+# # using escape sequences
+# def twice_repeated(string):
+#     return re.fullmatch("(.+)\\1", string)
