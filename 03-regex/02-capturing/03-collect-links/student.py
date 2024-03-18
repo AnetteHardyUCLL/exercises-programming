8bb22806b7c7c2ceb7da1495ec236f5e43c37323
Anette Hardy Sosa <anette.hardysosa@student.ucll.be>
2024-03-18 16:36:23 +0100

    solved exercises

--- 03-regex/02-capturing/03-collect-links/student.py
+++ 03-regex/02-capturing/03-collect-links/student.py
@@ -1,1 +1,12 @@
-# Write your code here
+import re
+
+
+def collect_links(string):
+    return re.findall(r'<a href="(.*)">', string)
+
+
+# The result depends on the number of capturing groups in the pattern
+# If there are no groups, return a list of strings matching the whole pattern
+# If there is exactly one group, return a list of strings matching that group
+# If multiple groups are present, return a list of tuples of strings matching the groups
+# Non-capturing groups do not affect the form of the result
